da8106293729198e31d070ed362488e09420f774
alxbaker <37917867+alxbaker@users.noreply.github.com>
2023-05-17 09:58:15 -0500

    Create functions to place markers and switch active player

--- scripts.js
+++ scripts.js
@@ -1,7 +1,54 @@
-const cells = document.querySelectorAll('.cell');
+const playerFactory  = (marker) => {
+  const getMarker = () => marker;
+  return {
+    getMarker
+  };
+};
 
-cells.forEach((cell) => {
-  cell.addEventListener('click', () => {
-    cell.textContent = 'X';      
+const displayControllerModule = (() => {
+  const cells = document.querySelectorAll('.cell');
+
+  cells.forEach((cell) => {
+    cell.addEventListener('click', () => {
+      gameControllerModule.takeTurn(cell);    
+    });
   });
-});
+})();
+
+const gameBoardModule =  (()  => {
+  const places = new Array(9);
+})();
+
+const gameControllerModule = (() => {
+  const playerX = playerFactory('X');
+  const playerO = playerFactory('O');
+  let activePlayer = playerX;
+
+  const placeMarker = (cell, marker) => {
+    // Check that the space is open
+    if (cell.textContent.length === 0) {
+      cell.textContent = marker;
+      swithcActivePlayer();
+    }
+  }
+
+  const swithcActivePlayer = () => {
+    activePlayer == playerX ? (activePlayer = playerO) : (activePlayer = playerX);
+  }
+
+  const checkWinner = () => {
+  }
+
+  const checkTie = () => {
+  }
+
+  const takeTurn = (cell) => {
+    marker = activePlayer.getMarker();
+    placeMarker(cell, marker);
+  }
+
+  return {
+    takeTurn
+  }
+})();
+
